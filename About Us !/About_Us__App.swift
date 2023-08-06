ddf01f6f970be3ff22db849694a07fbd9191c29e
mena <stuf408478@ese.gov.ae>
2023-08-06 15:40:35 +0400

    Initial Commit

---
+++ About Us !/About_Us__App.swift
@@ -0,0 +1,17 @@
+//
+//  About_Us__App.swift
+//  About Us !
+//
+//  Created by Mena Haitham on 06/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct About_Us__App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
